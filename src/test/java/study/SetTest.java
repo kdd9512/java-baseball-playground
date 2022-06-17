ff8886a35d29a519180147d78027a8889926dd49
kdd9512 <hyunwoo8805@gmail.com>
2022-06-17 23:53:59 +0900

    20220617 2nd commit
    Java 숫자야구게임 - 단위테스트
    실습 0번 진행중....

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,16 +1,23 @@
 package study;
 
+import org.assertj.core.api.AbstractAssert;
+import org.assertj.core.util.Strings;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SetTest {
 
-    private Set<Integer> numbers;
+    private static Set<Integer> numbers;
 
     @BeforeEach
     void setUp() {
@@ -19,7 +26,6 @@ public class SetTest {
         numbers.add(1);
         numbers.add(2);
         numbers.add(3);
-
     }
 
     @Test
@@ -27,6 +33,23 @@ public class SetTest {
         assertThat(numbers.contains(1)).isTrue();
         assertThat(numbers.contains(2)).isTrue();
         assertThat(numbers.contains(3)).isTrue();
+    }
+
+    public boolean isBlank(String input) {
+        return input.isEmpty();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "  "})
+    void isBlank_ShouldReturnTrueForNullOrBlankStrings(String input) {
+        assertTrue(isBlank(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"test:test", "tEst:test", "Java:java"}, delimiter = ':')
+    void toLowerCase_ShouldGenerateTheExpectedLowercaseValue(String input, String expected) {
+        String actualValue = input.toLowerCase();
+        assertEquals(expected, actualValue);
     }
 
 }
